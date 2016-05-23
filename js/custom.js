08cae840862f2e7eb193b026c7af0c1608af19ea
NickPW <NIck@Parinyas-MacBook-Pro.local>
2016-05-22 23:11:01 -0700

    add activation

--- js/custom.js
+++ js/custom.js
@@ -116,3 +116,25 @@ wow.init();
 function goBack() {
     window.history.back();
 }
+
+//activate
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight();
+ 
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+ 
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+ 
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+ 
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
